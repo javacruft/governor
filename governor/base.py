fcc80594f4ddfbacb2d6f02b9335b13cdf24e64c
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2020-11-20 13:15:57 +0100

    Add Juju wrapper and remove hardcoded governor
    Add a wrapper to avoid async libjuju in charm
    code.
    
    Signed-off-by: Dominik Fleischmann <dominik.fleischmann@canonical.com>

--- governor/base.py
+++ governor/base.py
@@ -2,7 +2,7 @@
 #
 # (c) 2020 Canonical Ltd. All right reservered
 #
-
+from .juju_wrapper import JujuConnection
 from juju.controller import Controller
 from ops.charm import CharmBase, EventBase, ObjectEvents, EventSource
 from ops.framework import StoredState, Object
@@ -64,6 +64,17 @@ class GovernorBase(CharmBase):
 
         self.state.set_default(model_name=model_name)
 
+        if self.creds_available():
+            self.juju = JujuConnection(
+                self.model.config["juju_controller_address"],
+                self.model.config["juju_controller_user"],
+                self.model.config["juju_controller_password"],
+                self.model.config["juju_controller_cacert"],
+                self.state.model_name,)
+        else:
+            self.juju = None
+
+
     def start_governord(self):
         model_name = self.state.model_name
         creds = {
@@ -72,6 +83,7 @@ class GovernorBase(CharmBase):
             "password": self.model.config["juju_controller_password"],
             "cacert": self.model.config["juju_controller_cacert"],
             "model": model_name,
+            "governor-charm": self.model.app.name,
         }
         with open("/var/snap/governor-broker/common/creds.yaml", "w") as creds_file:
             creds_file.write(yaml.dump(creds))
@@ -82,6 +94,17 @@ class GovernorBase(CharmBase):
 
         subprocess.run(["snap", "install", "governor-broker"], check=True)
 
+    def creds_available(self):
+        addr = self.model.config['juju_controller_address']
+        if len(addr) == 0:
+            addr = os.environ['JUJU_API_ADDRESSES'].split(" ")[0]
+        user = self.model.config['juju_controller_user']
+        password = self.model.config['juju_controller_password']
+        cacert = self.model.config['juju_controller_cacert']
+
+        return not (len(addr) == 0 or len(user) == 0 or len(password) == 0 or
+                len(cacert) == 0)
+ 
     def connected(f):
         async def wrapper(*args):
             ctrl, model = await args[0].connect_juju_components()
---
+++ governor/juju_wrapper.py
@@ -0,0 +1,77 @@
+
+from juju.controller import Controller
+from juju import loop
+from ops.model import ModelError
+import asyncio
+
+
+class JujuConnection:
+
+    def __init__(self, endpoint, username, password, cacert, model):
+        loop.run(self.connect_juju_components(endpoint,
+                                              username,
+                                              password,
+                                              cacert,
+                                              model))
+
+    async def connect_juju_components(self, endpoint, username, password, cacert, model):
+        self.ctrl = Controller()
+        await self.ctrl.connect(
+            endpoint=endpoint,
+            username=username,
+            password=password,
+            cacert=cacert,
+        )
+
+        self.model = await self.ctrl.get_model(model)
+
+    def model_execute(self, method, **kwargs):
+        function = getattr(self.model, method)
+
+        loop.run(function(**kwargs))
+
+    def application_execute(self, application, method, **kwargs):
+        function = getattr(application, method)
+
+        loop.run(function(**kwargs))
+
+    def execute_action(self, application_name, action_name, **kwargs):
+        loop.run(self._execute_action(application_name, action_name, **kwargs))
+
+    async def _execute_action(self, application_name, action_name, **kwargs):
+        if not self.model.applications and application_name not in self.model.applications:
+            return
+
+        application = self.model.applications[application_name]
+
+        for u in application.units:
+            if await u.is_leader_from_status():
+                unit = u
+
+        await unit.run_action(action_name, **kwargs)
+
+    def deploy(self, **kwargs):
+        loop.run(self.model.deploy(**kwargs))
+
+    def wait_for_deployment_to_settle(self, charm_name):
+        loop.run(self._wait_for_deployment_to_settle(charm_name))
+
+    async def _wait_for_deployment_to_settle(self, charm_name, allowed_workload_status=["active"]):
+        try:
+            filtered_apps = []
+            for name in self.model.applications:
+                if charm_name == name:
+                    continue
+                filtered_apps.append(self.model.applications[name])
+            await self.model.block_until(
+                lambda: all((unit.workload_status in allowed_workload_status) and
+                            unit.agent_status == 'idle'
+                            for application in filtered_apps
+                            for unit in application.units),
+                timeout=320)
+        except asyncio.TimeoutError:
+            raise ModelError(
+                'Timed out while waiting for deployment to finish')
+
+
+
